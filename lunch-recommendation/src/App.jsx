47fdfcf0c04eb0f057619940393c1eace8f70ae1
Kweon Ho Park <parkkweo@oregonstate.edu>
2023-11-13 17:40:56 -0800

    msg

--- lunch-recommendation/src/App.jsx
+++ lunch-recommendation/src/App.jsx
@@ -2,6 +2,7 @@
 import Home from "./components/Home"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
+import AppWeather from "./components/AppWeather"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from 'react';
 
@@ -10,9 +11,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
           <Route path="/home" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </Router>
     </div>
